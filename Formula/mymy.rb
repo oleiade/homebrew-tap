a616c5dfb9e1c214181bd0cba6077d30386ee807
oleiade <theo@crevon.me>
2023-04-24 22:51:00 +0200

    Add mymy v0.3.0

--- Formula/mymy.rb
+++ Formula/mymy.rb
@@ -1,15 +1,15 @@
 class Mymy < Formula
   desc "Get essential information about your device"
   homepage "https://github.com/oleiade/mymy"
-  version "v0.2.0"
+  version "v0.3.0"
   license "AGPL-3.0 license"
 
   if Hardware::CPU.arm?
-    url "https://github.com/oleiade/mymy/releases/download/v0.2.0/my-aarch64-apple-darwin.tar.gz"
-    sha256 "734f9572c4da9f92602fba291a23ace74949368ca3ec44737b810042aaa78b65"
+    url "https://github.com/oleiade/mymy/releases/download/v0.3.0/my-aarch64-apple-darwin.tar.gz"
+    sha256 "21aab04870d855bf6ed8adfa111f28a061f157270ac6a536452ad96db33b7364"
   else
-    url "https://github.com/oleiade/mymy/releases/download/v0.2.0/my-x86_64-apple-darwin.tar.gz"
-    sha256 "82f9b1b9ddabd7b880d1704e34fed55571e9ebd7936898ff6def73b8c51ae0c1"
+    url "https://github.com/oleiade/mymy/releases/download/v0.3.0/my-x86_64-apple-darwin.tar.gz"
+    sha256 "b079058652e62d2867387f8c8f66f938b29b7fe571607e4b08dd1210429a396d"
   end
 
   def install
